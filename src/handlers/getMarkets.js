09b4a806b5d1b104fc44b8dd9d723dec0f5bde85
estebanpablo89 <estebanpablo89@gmail.com>
2021-03-18 11:27:04 -0500

    crud get markets

--- src/handlers/getMarkets.js
+++ src/handlers/getMarkets.js
@@ -1,4 +1,3 @@
-import { v4 as uuid } from 'uuid';
 import AWS from 'aws-sdk';
 import middy from '@middy/core';
 import httpJsonBodyParser from '@middy/http-json-body-parser';
@@ -8,48 +7,28 @@ import createError from 'http-errors';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
-async function createMarket(event, context) {
-  const {
-    country,
-    currency,
-    code_symbol,
-    show_cents,
-    display,
-  } = event.body;
-
-  const now = new Date();
-
-  const market = {
-    id: uuid(),
-    country,
-    currency,
-    format: {
-      code_symbol,
-      show_cents,
-      display,
-    },
-    createdAt: now.toISOString(),
-  };
+async function getMarkets(event, context) {
+  let markets;
 
   try {
-    await dynamodb
-      .put({
+    const result = await dynamodb
+      .scan({
         TableName: process.env.MARKETS_TABLE_NAME,
-        Item: market,
       })
       .promise();
+
+    markets = result.Items;
   } catch (error) {
     console.error(error);
     throw new createError.InternalServerError(error);
   }
-
   return {
-    statusCode: 201,
-    body: JSON.stringify(market),
+    statusCode: 200,
+    body: JSON.stringify(markets),
   };
 }
 
-export const handler = middy(createMarket)
+export const handler = middy(getMarkets)
   .use(httpJsonBodyParser())
   .use(httpEventNormalizer())
   .use(httpErrorHandler());
