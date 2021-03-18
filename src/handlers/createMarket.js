b894b89c2ae96d55f97b514e3f3b36380709ee4e
estebanpablo89 <estebanpablo89@gmail.com>
2021-03-18 10:42:45 -0500

    create market

---
+++ src/handlers/createMarket.js
@@ -0,0 +1,42 @@
+import { v4 as uuid } from 'uuid';
+import AWS from 'aws-sdk';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function createMarket(event, context) {
+  const {
+    country,
+    currency,
+    code_symbol,
+    show_cents,
+    display,
+  } = JSON.parse(event.body);
+
+  const now = new Date();
+
+  const market = {
+    id: uuid(),
+    country,
+    currency,
+    format: {
+      code_symbol,
+      show_cents,
+      display,
+    },
+    createdAt: now.toISOString(),
+  };
+
+  await dynamodb
+    .put({
+      TableName: 'MarketsTable',
+      Item: market,
+    })
+    .promise();
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify(market),
+  };
+}
+
+export const handler = createMarket;
--- src/handlers/hello.js
+++
@@ -1,8 +0,0 @@
-async function hello(event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Hey!' }),
-  };
-}
-
-export const handler = hello;
